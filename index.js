7db21ff8c859ffd785af67d5e24715432c6f2231
Kamran Ahmed <kamranahmed.se@gmail.com>
2016-12-03 02:20:23 +0400

    Update package structure and minor fix

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function ApiProblem(statusCode, title, description, additional) {
 
   additional = additional || {};
 
-  this.status = parseInt(statusCode || httpStatus.STATUS_CODES, 10);
+  this.status = parseInt(statusCode || httpStatus.INTERNAL_SERVER_ERROR, 10);
   this.title = title || httpStatus.getStatusText(this.status) || 'Unknown error';
   this.description = description || this.title;
-  this.type = type || 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html';
+  this.type = additional.type || 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html';
 
   Object.assign(this, additional);
 }
