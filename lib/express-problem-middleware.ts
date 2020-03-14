a1dce43ab36d45d5c4f3da53d27b812ccdb9a3bc
Kamran Ahmed <kamranahmed.se@gmail.com>
2020-03-14 19:04:05 +0400

    Add more tests for problem middleware

--- lib/express-problem-middleware.ts
+++ lib/express-problem-middleware.ts
@@ -3,27 +3,47 @@ import { NextFunction, Request, Response } from 'express';
 import { ErrorRequestHandler } from 'express-serve-static-core';
 import ApiProblem from './api-problem';
 
-const contentType: string = 'application/problem+json';
+type MiddlewareOptionsType = { stackTrace?: boolean; contentType?: string; };
+type ExpressProblemMiddlewareType = (options?: MiddlewareOptionsType) => ErrorRequestHandler
 
-const ExpressProblemMiddleware: ErrorRequestHandler = function (err: any, req: Request, res: Response, next: NextFunction) {
-  if (err instanceof ApiProblem) {
-    res.status(err.status)
-      .header('Content-Type', contentType)
-      .json(JSON.stringify(err));
-  } else if (err instanceof Error) {
-    const error: ApiProblem = new ApiProblem({
-      status: INTERNAL_SERVER_ERROR,
-      additional: {
-        stack: err?.stack?.split('\n') || undefined
-      }
-    });
+const ExpressProblemMiddleware: ExpressProblemMiddlewareType = function (options: MiddlewareOptionsType = {}): ErrorRequestHandler {
 
-    res.status(error.status)
-      .header('Content-Type', contentType)
-      .json(JSON.stringify(error));
-  } else {
+  // Merge the options with defaults
+  options = {
+    stackTrace: true,
+    contentType: 'application/problem+json',
+    ...options
+  };
+
+  return function (err: any, req: Request, res: Response, next: NextFunction) {
+    // For API problems, just respond with the error
+    if (err instanceof ApiProblem) {
+      res.status(err.status)
+        .header('Content-Type', options.contentType)
+        .json(JSON.stringify(err));
+
+      return;
+    }
+
+    // For Exceptions, create internal error exception
+    if (err instanceof Error) {
+      const error: ApiProblem = new ApiProblem({
+        status: INTERNAL_SERVER_ERROR,
+        description: err.message,
+        additional: (options.stackTrace ? {
+          stack: err?.stack || undefined
+        } : undefined)
+      });
+
+      res.status(error.status)
+        .header('Content-Type', options.contentType)
+        .json(JSON.stringify(error));
+
+      return;
+    }
+
     next();
-  }
+  };
 };
 
 export default ExpressProblemMiddleware;
--- tests/middleware.test.ts
+++ tests/middleware.test.ts
@@ -24,6 +24,7 @@ describe('middleware', () => {
   }
 
   beforeAll(setUpExpressMocks);
+  afterEach(() => jest.clearAllMocks());
 
   it.each([
     [undefined],
@@ -37,7 +38,7 @@ describe('middleware', () => {
     const res: Response = jest.fn() as any;
     const next: NextFunction = jest.fn() as any;
 
-    ExpressProblemMiddleware(errorParam, req, res, next);
+    ExpressProblemMiddleware()(errorParam, req, res, next);
 
     expect(req).not.toHaveBeenCalled();
     expect(res).not.toHaveBeenCalled();
@@ -56,7 +57,7 @@ describe('middleware', () => {
       additional: {}
     };
 
-    ExpressProblemMiddleware(new ApiProblem(problemParams), req, res, next);
+    ExpressProblemMiddleware()(new ApiProblem(problemParams), req, res, next);
 
     expect(next).not.toHaveBeenCalled();
 
@@ -67,5 +68,55 @@ describe('middleware', () => {
     expect(resHeader).toHaveBeenCalledWith('Content-Type', 'application/problem+json');
     expect(resStatus).toHaveBeenCalledWith(400);
     expect(resJson).toHaveBeenCalledWith(JSON.stringify(problemParams));
+  });
+
+  it('should respond with API problem response for Error', () => {
+    const req: Request = jest.fn() as any;
+    const next: NextFunction = jest.fn() as any;
+
+    const problemParams: IApiProblem = {
+      status: 500,
+      title: 'Server Error',
+      description: 'Database connection failed',
+      type: 'https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html'
+    };
+
+    ExpressProblemMiddleware({ stackTrace: false })(new Error('Database connection failed'), req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+
+    expect(resHeader).toHaveBeenCalledTimes(1);
+    expect(resStatus).toHaveBeenCalledTimes(1);
+    expect(resJson).toHaveBeenCalledTimes(1);
+
+    expect(resHeader).toHaveBeenCalledWith('Content-Type', 'application/problem+json');
+    expect(resStatus).toHaveBeenCalledWith(500);
+    expect(resJson).toHaveBeenCalledWith(JSON.stringify(problemParams));
+  });
+
+  it('should honor the options passed', () => {
+    const req: Request = jest.fn() as any;
+    const next: NextFunction = jest.fn() as any;
+
+    ExpressProblemMiddleware({
+      stackTrace: true,
+      contentType: 'some-content-type'
+    })(new Error('Database connection failed'), req, res, next);
+
+    expect(next).not.toHaveBeenCalled();
+
+    expect(resHeader).toHaveBeenCalledTimes(1);
+    expect(resStatus).toHaveBeenCalledTimes(1);
+    expect(resJson).toHaveBeenCalledTimes(1);
+
+    expect(resHeader).toHaveBeenCalledWith('Content-Type', 'some-content-type');
+    expect(resStatus).toHaveBeenCalledWith(500);
+
+    // Because we have the stack trace enabled, we can't have the exact "expect" match
+    // @todo write custom matcher and match the object format without stack
+    expect(resJson).toHaveBeenCalledWith(expect.stringContaining('Database connection failed'));
+    expect(resJson).toHaveBeenCalledWith(expect.stringContaining('Server Error'));
+    expect(resJson).toHaveBeenCalledWith(expect.stringContaining('https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html'));
+    expect(resJson).toHaveBeenCalledWith(expect.stringContaining('500'));
   });
 });
